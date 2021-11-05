c98c20703f17f45135828681cb3ddd67a6412bcc
VladKocherga <vlad.kocherga228@gmail.com>
2021-11-05 13:10:27 +0300

    fix: fix code

--- module/code.js
+++ module/code.js
@@ -1,5 +1,7 @@
 module.exports.phoneValid = function phoneValid(number, shift) {
   if (shift == 1) {
+    number = number.replace(/\D/g, "");
+
     let arr = [];
     arr.unshift("(");
     for (let i = 0; i < number.length; i++) {
@@ -18,19 +20,9 @@ module.exports.phoneValid = function phoneValid(number, shift) {
 
     return "\n" + arr.join("");
   }
+  if (shift == 2) {
+    arr = new Set([...myArray].filter((x) => !toRemove.has(x)));
 
-  //   let output = "";
-  //   for (let i = 0; i < str.length; i++) {
-  //     let c = str[i];
-  //     if (c.match(/[a-z]/i)) {
-  //       const code = str.charCodeAt(i);
-  //       if (code >= 65 && code <= 90) {
-  //         c = String.fromCharCode(((code - 65 + shift) % 26) + 65);
-  //       } else if (code >= 97 && code <= 122) {
-  //         c = String.fromCharCode(((code - 97 + shift) % 26) + 97);
-  //       }
-  //     }
-  //     output += c;
-  //   }
-  //   return `${output}`;
+    return Array.from(difference);
+  }
 };
